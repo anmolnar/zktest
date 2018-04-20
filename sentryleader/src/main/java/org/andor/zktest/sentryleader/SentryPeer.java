5c7efa2e25598287d15374aafa81ad93b028d3ba
Andor Molnar <andor@cloudera.com>
2018-04-20 11:56:38 +0200

    Added maven project structure

---
+++ sentryleader/src/main/java/org/andor/zktest/sentryleader/SentryPeer.java
@@ -0,0 +1,11 @@
+package org.andor.zktest.sentryleader;
+
+public class SentryPeer {
+
+    public static void main(String[] args) {
+        System.out.println("Hi, this is Sentry test peer");
+
+
+    }
+
+}
