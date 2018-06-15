1e1482796a9d0a2ab155f3ea418404a13de31f2d
Andor Molnar <andor@cloudera.com>
2018-06-15 19:26:56 +0200

    Added Zoomba project

--- zoomba/src/main/java/org/andor/zoomba/ZoombaMain.java
+++ zoomba/src/main/java/org/andor/zoomba/ZoombaMain.java
@@ -1,3 +1,5 @@
+package org.andor.zoomba;
+
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -10,6 +12,20 @@ public class ZoombaMain {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
         System.out.println("Zoomba starts");
 
+        String javaVersion = System.getProperty("java.version");
+        System.out.println("Java version = " + javaVersion);
+
+        String[] versionParts = javaVersion.split("\\.");
+        int majorVersion = Integer.parseInt(versionParts[0]);
+        int minorVersion = Integer.parseInt(versionParts[1]);
+
+        if (majorVersion >= 9) {
+            System.out.println("This is at least Java 9");
+        } else {
+            System.out.println("This is before Java 9");
+        }
+
+
         SSLContext context = SSLContext.getDefault();
 
         SSLSocketFactory socketFactory = context.getSocketFactory();
@@ -19,5 +35,8 @@ public class ZoombaMain {
         System.out.println("Provider = " + context.getProvider());
         System.out.println("Protocol = " + context.getProtocol());
         System.out.println("Enabled protocols = " + Arrays.toString(socket.getEnabledProtocols()));
+
+
+
     }
 }
